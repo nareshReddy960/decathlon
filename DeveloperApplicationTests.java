eedb128c6f0a5caa22b019ad142868cb1bb3cf2e
nareshReddy960 <80030838+nareshReddy960@users.noreply.github.com>
2021-03-04 14:10:50 +0545

    Add files via upload

---
+++ DeveloperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decathlon.developer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeveloperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
